4d7bd3bba1c202ad729980201dab759670bb5e41
Abdellah-Nsila <dawndawn074@gmail.com>
2025-11-28 21:39:19 +0100

    Add lspconfig plugin for server-editor-interation

--- init.lua
+++ init.lua
@@ -1,8 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.opt.shiftwidth = 4
+vim.clipboard = "unnamedplus"
+vim.number = true
+
 if true then
-	print("TODO: Push your nvim-setup-config Repo")
+    print("TODO: Push your nvim-setup-config Repo")
 end
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 
 
