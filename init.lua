dc2b8d76c03732c3afb70ea37d6e1f18c67502a4
Abdellah-Nsila <dawndawn074@gmail.com>
2025-11-30 22:52:42 +0100

    Add Telescope plugin

--- init.lua
+++ init.lua
@@ -2,8 +2,33 @@
 require("config.lazy")
 
 vim.opt.shiftwidth = 4
-vim.clipboard = "unnamedplus"
-vim.number = true
-vim.tabstop = 2
+vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
 
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+
+vim.api.nvim_create_autocmd('TermOpen', {
+	group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+	callback = function()
+		vim.opt.number = false
+		vim.opt.relativenumber = false
+	end
+})
+
+local job_id = 0
+vim.keymap.set("n", "<space>st", function()
+	vim.cmd.vnew()
+	vim.cmd.term()
+	vim.cmd.wincmd("J")
+	vim.api.nvim_win_set_height(0, 15)
+
+	job_id = vim.bo.channel
+end)
+
+vim.keymap.set("n", "<space>example", function()
+	vim.fn.chansend(job_id, { "ls -al\r\n" })
+end)
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -43,29 +43,29 @@ return {
 			vim.api.nvim_create_autocmd('LspAttach', {
 				group = vim.api.nvim_create_augroup('my.lsp', {}),
 				callback = function(args)
-					local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-					-- if client:supports_method('textDocument/implementation') then
+					local c = assert(vim.lsp.get_client_by_id(args.data.client_id))
+					-- if c:supports_method('textDocument/implementation') then
 					-- Create a keymap for vim.lsp.buf.implementation ...
 					-- end
 
 					-- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
-					-- if client:supports_method('textDocument/completion') then
+					-- if c:supports_method('textDocument/completion') then
 					-- Optional: trigger autocompletion on EVERY keypress. May be slow!
 					-- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-					-- client.server_capabilities.completionProvider.triggerCharacters = chars
+					-- c.server_capabilities.completionProvider.triggerCharacters = chars
 
-					--	vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+					--	vim.lsp.completion.enable(true, c.id, args.buf, { autotrigger = true })
 					-- end
 
 					-- Auto-format ("lint") on save.
 					-- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-					if not client:supports_method('textDocument/willSaveWaitUntil')
-						and client:supports_method('textDocument/formatting') then
+					if not c:supports_method('textDocument/willSaveWaitUntil')
+						and c:supports_method('textDocument/formatting') then
 						vim.api.nvim_create_autocmd('BufWritePre', {
 							group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
 							buffer = args.buf,
 							callback = function()
-								vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+								vim.lsp.buf.format({ bufnr = args.buf, id = c.id, timeout_ms = 1000 })
 							end,
 						})
 					end
