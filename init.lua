bdc541b7ceaf07dc823ce8b9b67046346ef2b711
Abdellah-Nsila <dawndawn074@gmail.com>
2025-12-01 17:54:46 +0100

    Some Fancy stuff

--- init.lua
+++ init.lua
@@ -6,11 +6,26 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 2
+vim.opt.signcolumn = 'yes'
+vim.opt.scrolloff = 10
+vim.opt.list = true
+vim.opt.listchars = { tab = '→  ', trail = '*', space = '.' }
 
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
-vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+vim.keymap.set("n", "<m-k>", "<cmd>cprev<CR>")
 
+vim.keymap.set("n", "<ESC>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Hightlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
 vim.api.nvim_create_autocmd('TermOpen', {
 	group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
 	callback = function()
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -23,6 +23,7 @@ return {
 
 			vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
 			vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+			vim.keymap.set("n", "<space>gf", require('telescope.builtin').git_files)
 			vim.keymap.set("n", "<space>en", function()
 				require('telescope.builtin').find_files {
 					cwd = vim.fn.stdpath('config')
